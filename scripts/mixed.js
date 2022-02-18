0ee19f5348c9916a96e4aa61453fc56292077d26
Sercan Sebetçi <sercansebetci@gmail.com>
2022-02-19 01:10:24 +0300

    fix: mobile compatibility issues fixes

--- scripts/mixed.js
+++ scripts/mixed.js
@@ -4,12 +4,13 @@
 
     searchButton.addEventListener("click", function(e) {
         $('#snipper').css("display", "block");
-        
+
         setTimeout(function() {
-            $('#snipper').css("display", "none");    
+            $('#snipper').css("display", "none");
+            $('#searchButton').popover('hide');
         }, 5000);
     });
-    
+
     $(function () {
       $('[data-toggle="popover"]').popover({ container: 'body' });
     });
