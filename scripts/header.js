df4017ef79872cde9e8d24ea917bb7959576da6b
Sercan Sebetçi <sercansebetci@gmail.com>
2022-02-19 16:34:15 +0300

    add: new features

--- scripts/header.js
+++ scripts/header.js
@@ -5,14 +5,14 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("changed");
 });
 
-window.onscroll = function () { 
+window.onscroll = function () {
     "use strict";
-    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) 
+    if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 )
     {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
-    } 
-    else 
+    }
+    else
     {
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
--- scripts/mixed.js
+++ scripts/mixed.js
@@ -1,18 +1,74 @@
-(function() {
-    const searchButton = document.getElementById("searchButton");
-    const mybutton = document.getElementById("btn-back-to-top");
+$( document ).ready(function() {
+  /**
+   * @brief Settings to use in Toastr notifications
+   */
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 
-    searchButton.addEventListener("click", function(e) {
-        $('#snipper').css("display", "block");
+  /**
+   * @brief Procedures to be called when the click event of the "Search" button is fired.
+   */
+  $('#searchButton').on('click', function (e) {
+    $('#snipper').css("display", "block");
 
-        setTimeout(function() {
-            $('#snipper').css("display", "none");
-            $('#searchButton').popover('hide');
-        }, 5000);
-    });
+    setTimeout(function () {
+      $('#snipper').css("display", "none");
+      $('#searchButton').popover('hide');
+    }, 5000);
+  });
 
-    $(function () {
-      $('[data-toggle="popover"]').popover({ container: 'body' });
-    });
-})();
+  /**
+   * @brief Anonymous method used for popover settings.
+   */
+  $(function () {
+    $('[data-toggle="popover"]').popover({ container: 'body' });
+  });
 
+  /**
+   * @brief Animation to be performed when clicking <a> items in the navigation menu
+   */
+  $('.nav-link').on('click', function (event) {
+    $('#snipper2').css("display", "block");
+
+    setTimeout(function () {
+      $('.nav-link').popover('hide');
+      $('#snipper2').css("display", "none");
+    }, 5000);
+  });
+
+  /**
+   * @brief Notification to be shown when clicking the "Home" button in the navigayion menu
+   */
+  $('#home-page').click(function (event) {
+    toastr["success"]('You are already on the current page.', "Notifying");
+  });
+
+  /**
+   * @brief Notifications to be shown when clicking other <a> items in the navigation menu
+   */
+  $('#project-page, #experience-page, #research-page, #about-page, #blog-page').click(function (event) {
+    toastr["error"]('This page is not yet available. Please try again later.', "Warning");
+  });
+
+  /**
+   * @brief Notification message to be shown when clicking the profile picture
+   */
+  $('#profile-image').click(function (event) {
+    toastr["info"]('Mechatronic Engineer', "Sercan Sebetçi");
+  });
+});
